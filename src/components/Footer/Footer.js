81436650e8c9b23be262f6b902174dd4f0be53e6
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-01-24 18:06:39 -0300

    fix links in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,18 +4,14 @@ import * as styles from './Footer.module.scss'
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <a target="_blank" rel="noopener noreferrer" href="https://georgetown.edu/">
+    <a target="_blank" href="https://georgetown.edu/">
       <img
         src="/img/GHSS logo.png"
         alt="Georgetown University Logo"
         className={styles.georgetown}
       />
     </a>
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
-      href="http://talusanalytics.com//"
-    >
+    <a target="_blank" href="https://ghssidea.org/">
       <img src="/img/IDEA_Logo.png" alt="Talus Analytics Logo" />
     </a>
   </footer>
